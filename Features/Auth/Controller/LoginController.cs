72e2bbad3c1b10ac350ff0eaec1eaca5ef31a499
Aradhya Nepal <aradhya.1221@gmail.com>
2024-01-15 10:59:37 +0545

    Initial Commit

---
+++ Features/Auth/Controller/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PocketA3.Features.Auth.Controller
+{
+    [Route("api/login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        public OkResult getValue() {
+            return Ok();
+        }
+    }
+}
